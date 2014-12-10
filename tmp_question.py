548bc500aa1e931b93a9b82aea54a0717a12e72a
Christoph Siedentop <christoph.siedentop@iwr.uni-heidelberg.de>
2014-12-10 15:41:50 +0100

    removed dependency on pycallgraph and no outputfiles are generated.

--- tmp_question.py
+++ tmp_question.py
@@ -1,55 +1,60 @@
-    import unittest
-    from pycallgraph import PyCallGraph
-    from pycallgraph.output import GraphvizOutput
+import unittest
+#from pycallgraph import PyCallGraph
+#from pycallgraph.output import GraphvizOutput
 
-    class Callgraph:
-        def __init__(self, output_file='callgraph.png'):
-            self.graphviz = GraphvizOutput()
-            self.graphviz.output_file = output_file
-        
-        def execute(self, function, *args, **kwargs):
-            with PyCallGraph(output=self.graphviz):
-                ret = function(*args, **kwargs)
+from tracer import Tracer
 
-            self.graph = dict()
-            for node in self.graphviz.processor.nodes():
-                if node.name != '__main__':
-                    f = eval(node.name)
-                    self.graph[node.name] = hash(f.__code__.co_code)
-            return ret
+class Callgraph:
+    def __init__(self, threaded = False):
+        self.threaded = threaded
+    
+    def execute(self, function, *args, **kwargs):
+        output = None
+        tracer = Tracer(output, self.threaded)
+        with tracer:
+            ret = function(*args, **kwargs)
 
-        def unchanged(self):
-            '''Checks each function in the callgraph whether it has changed.
-            Returns True if all the function have their original code-hash. False otherwise.
-            '''
-            for func, codehash in self.graph.iteritems():
-                f = eval(func)
-                if hash(f.__code__.co_code) != codehash:
-                    return False
-            return True
+        self.graph = dict()
+        for node in tracer.nodes():
+            if node.name != '__main__' and not node.name.startswith('tracer'):
+                self.graph[node.name] = node.hash
+        return ret
 
-    def func_inner(x):
-        return x
-    def func_outer(x):
-        return 2*func_inner(x)
+    def unchanged(self):
+        '''Checks each function in the callgraph whether it has changed.
+        Returns True if all the function have their original code-hash. False otherwise.
+        '''
+        for func, codehash in self.graph.iteritems():
+            f = eval(func)
+            if hash(f.__code__.co_code) != codehash:
+                return False
+        return True
 
-    class CallgraphTest(unittest.TestCase):
-        def testChanges(self):
-            cg = Callgraph()
-            y = cg.execute(func_outer, 3)
-            self.assertEqual(6, y)
-            self.assertTrue(cg.unchanged())
-            # Change one of the functions
-            def func_inner(x):
-                return 3+x
-            self.assertFalse(cg.unchanged())
-            # Change back!
-            def func_inner(x):
-                return x
-            self.assertTrue(cg.unchanged())
+def func_inner(x):
+    return x
+def func_outer(x):
+    return 2*func_inner(x)
 
 
-    if __name__ == '__main__':
-        unittest.main()
+class CallgraphTest(unittest.TestCase):
+    def testChanges(self):
+        global func_inner
+        global func_outer
+        cg = Callgraph()
+        y = cg.execute(func_outer, 3)
+        self.assertEqual(6, y)
+        self.assertTrue(cg.unchanged())
+        # Change one of the functions   
+        def func_inner(x):
+            return 3+x
+        self.assertEqual(12, func_outer(3))
+        self.assertFalse(cg.unchanged())
+        # Change back!
+        def func_inner(x):
+            return x
+        self.assertEqual(6, func_outer(3))
+        self.assertTrue(cg.unchanged())
 
 
+if __name__ == '__main__':
+    unittest.main()
